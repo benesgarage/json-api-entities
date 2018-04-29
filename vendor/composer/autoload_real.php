7fbf67cd1bc386e8b282509f89b56af78a5aa70a
benesgarage <benesbigkay@gmail.com>
2018-04-29 03:05:26 +0200

    Remove vendor files and folders from VCS.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInitcb3cd54013c06a6d76faec74299c0f6a
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitcb3cd54013c06a6d76faec74299c0f6a::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequirecb3cd54013c06a6d76faec74299c0f6a($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequirecb3cd54013c06a6d76faec74299c0f6a($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
